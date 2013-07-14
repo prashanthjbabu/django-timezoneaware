2f72a03b75e3a6808954ea2d52cea5281255352e
prashanth <prashanthjbabu@gmail.com>
2013-07-14 09:52:05 +0530

    stats import

--- timezoneaware/settings.py
+++ timezoneaware/settings.py
@@ -108,7 +108,7 @@ ROOT_URLCONF = 'timezoneaware.urls'
 WSGI_APPLICATION = 'timezoneaware.wsgi.application'
 
 TEMPLATE_DIRS = (
-    "/static"
+    "/static",
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
