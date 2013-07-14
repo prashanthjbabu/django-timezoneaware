3818d07086cb5af6bfbee324a73e5df4cb37021e
prashanth <prashanthjbabu@gmail.com>
2013-07-14 09:56:08 +0530

    stats import

--- timezoneaware/settings.py
+++ timezoneaware/settings.py
@@ -61,7 +61,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/var/www/timezoneaware/timezoneaware/static'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -108,7 +108,8 @@ ROOT_URLCONF = 'timezoneaware.urls'
 WSGI_APPLICATION = 'timezoneaware.wsgi.application'
 import os
 TEMPLATE_DIRS = (
-    os.path.join(os.path.dirname(__file__), './static').replace('\\','/'),
+    "/var/www/timezoneaware/timezoneaware/static"
+    #os.path.join(os.path.dirname(__file__), './static').replace('\\','/'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
