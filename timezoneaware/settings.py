f3b54025db9184174bbbaf243dc3a90138f4d3cd
prashanth <prashanthjbabu@gmail.com>
2013-08-02 11:25:08 +0530

    pass

--- timezoneaware/settings.py
+++ timezoneaware/settings.py
@@ -15,7 +15,7 @@ DATABASES = {
         'NAME': 'timezoneaware',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'insignia',
         'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
     }
